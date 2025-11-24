a2a046af671052a8d816fd30d13880d7b29ee03d
Robbe Rollez <robbe.rollez@student.vives.be>
2025-11-24 17:07:59 +0100

    File updates

--- start.bat
+++ start.bat
@@ -4,6 +4,11 @@ set -e
 # Optioneel: laatste versie van images binnenhalen
 docker compose pull
 
+# InfluxDB data leegmaken zodat setup/init opnieuw lopen
+echo "Verwijderen van InfluxDB data..."
+rm -rf ./influxdb/data/
+mkdir -p ./influxdb/data/
+
 # (Re)build custom images (Node-RED)
 docker compose build
 
